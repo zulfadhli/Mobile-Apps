7cdba2d7f36866d333471a3d0efed6b2a528e301
unknown <ohzulfadhli@gmail.com>
2014-01-25 12:13:44 +0800

    chaneg index

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+	$id=$_GET['id'];
+	$displayarray("Ini No 0","Ini No 2","Ini No 3","Ini No 4","Ini No 5","Ini No 6");
+
+?>
+<html>
+<head>
+	<title>MobileApps</title>
+</head>
+<body>
+<center>
+	<? echo $displayarray[$id];?><br>
+	<a href"index.php?id=1">1</a><br>
+	<a href"index.php?id=1">2</a><br>
+	<a href"index.php?id=1">3</a><br>
+	<a href"index.php?id=1">4</a><br>
+	<a href"index.php?id=1">5</a><br>
+	<a href"index.php?id=1">6</a><br>
+</center>
+</body>
+</html>
