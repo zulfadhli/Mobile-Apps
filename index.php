2cd3f1b48af7a81ec3cd43138d289ccdabb893fc
unknown <ohzulfadhli@gmail.com>
2014-01-25 12:23:09 +0800

    change index

--- index.php
+++ index.php
@@ -10,12 +10,12 @@
 <body>
 <center>
 	<? echo $displayarray[$id];?><br>
-	<a href"index.php?id=1">1</a><br>
-	<a href"index.php?id=1">2</a><br>
-	<a href"index.php?id=1">3</a><br>
-	<a href"index.php?id=1">4</a><br>
-	<a href"index.php?id=1">5</a><br>
-	<a href"index.php?id=1">6</a><br>
+	<a href="index.php?id=1">1</a><br>
+	<a href="index.php?id=1">2</a><br>
+	<a href="index.php?id=1">3</a><br>
+	<a href="index.php?id=1">4</a><br>
+	<a href="index.php?id=1">5</a><br>
+	<a href="index.php?id=1">6</a><br>
 </center>
 </body>
 </html>
